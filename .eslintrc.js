2348dc6b8590cec37f6fc22603d139d42bb3a457
Paul Ferguson <paullferguson@gmail.com>
2020-04-30 10:51:03 -0700

    Configure Jest, eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,24 @@
 
 module.exports = {
-  "extends": "airbnb-base",
-  "rules" : {
-    "no-console": "off",
+  parserOptions: {
+    ecmaFeatures: {
+        jsx: true
+    }
+  },
+  configs: {
+    airbnbConfig : {
+      "extends": "airbnb-base",
+      "rules" : {
+        "no-console": "off",
+      },
+    },
+    jestConfig: {
+      "plugins": ["jest"],
+      "rules": {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+      }
+    }
   }
 };
