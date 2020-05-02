2581f5e5bb2d61877d246edaaa41871ec6f6fbf3
Paul Ferguson <paullferguson@gmail.com>
2020-05-02 16:44:00 -0700

    Adds the Axios libiry and set the response to the state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,12 +1,26 @@
 
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      reviews: []
     };
+  }
+
+  componentDidMount() {
+    let dummyId = '017';
+    axios.get(`/api/v1/products/${dummyId}/reviews`)
+      .then(data => {
+        this.setState({
+          reviews: data.data.reviews
+        });
+      })
+      .catch(err => console.log('Error: GET reviews: ', err));
+
   }
 
   render() {
