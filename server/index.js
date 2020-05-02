c13ba8d7828e704519ed1e3fbce1ee5aef12ebb2
Paul Ferguson <paullferguson@gmail.com>
2020-05-01 22:14:11 -0700

    Build up the response object for GET /products/:products/reviews

--- server/index.js
+++ server/index.js
@@ -1,24 +1,54 @@
 
 const express = require('express');
+const { Review, Product } = require('../database/index');
 
-const Review = express();
-Review.set('port', 3003);
+const ReviewApp = express();
+ReviewApp.set('port', 3003);
 
 // The Middle
-Review.use(express.json());
-Review.use((req, resp, next) => {
+ReviewApp.use(express.json());
+ReviewApp.use((req, resp, next) => {
   console.log(`${req.method}:${req.originalUrl}`);
   next();
 });
 
 // Static
-Review.use(express.static('public'));
+ReviewApp.use(express.static('public'));
 
 
 // Routes
+ReviewApp.get(`/api/v1/products/:product_id/reviews`, (req, resp) => {
+  Product.find({ product_id: `${req.params.product_id}` })
+    .then((data) => {
 
+      // get the array of review ids
+      let revData = data[0].product_reviews;
+      let prodOpt = data[0].product_option;
+
+      Review.findById(revData[0], (err, data) => {
+        if (err) { throw err; }
+        else {
+
+          data = {
+            ...data._doc,
+            product_id: req.params.product_id,
+            product_options: prodOpt
+          };
+
+          resp.status(200).send({
+            reviews: data
+          });
+          return data;
+        }
+      })
+
+    })
+    .catch((err) => {
+      resp.status(500).send(err);
+    });
+})
 
 // LetLive.
-Review.listen(Review.set('port'), () => {
-  console.log(`Review Server running: ${Review.set('port')}`);
+ReviewApp.listen(ReviewApp.set('port'), () => {
+  console.log(`ReviewApp Server running: ${ReviewApp.set('port')}`);
 });
