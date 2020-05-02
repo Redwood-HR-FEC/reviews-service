335873a94b7bdbadac76e6d1845cd4aad626f759
Paul Ferguson <paullferguson@gmail.com>
2020-05-02 16:16:49 -0700

    Tidy up the controller

--- server/controller/getReviewsByProductId.js
+++ server/controller/getReviewsByProductId.js
@@ -13,77 +13,38 @@ module.exports = {
         let revArr = data[0].product_reviews;
         let prodOpt = data[0].product_option;
 
-        // console.log('revArr: ', revArr);
-        // revArr = [1, 2, 3, 4, 5];
-
         const findReviews = (revId) => {
           console.log('revId: ', revId);
           return new Promise(resolve => {
-            // setTimeout(() => {
-            //   resolve(revId * 2)
-            // }, 100);
-
-            // return resolve = () => {
-              return Review.findById(revId, (err, data) => {
-                if (err) throw err
-                if (!err) {
-
+            return Review.findById(revId, (err, data) => {
+              if (err) throw err
+              if (!err) {
+                if (data) {
                   data = {
                     ...data._doc,
                     product_id: req.params.product_id,
                     product_options: prodOpt
                   };
-
-                  console.log('data', data);
-
                   resolve(data);
+                } else {
+                  resp.status(500).send(err);
+                  throw Error ('No data._doc matches the review ID');
                 }
-              })
-            // }
+              }
+            })
           });
         }
 
+        // Map over each ID and create a promise
         const revObjPro = revArr.map(findReviews);
         const revResults = Promise.all(revObjPro);
 
+        // Send it!
         revResults.then(data => {
           resp.status(200).send({
             reviews: data
           });
         })
-
-        // findReviews(revArr)
-        //   .then(revObj => {
-        //     revObjArr.push(revObj);
-
-        //     // console.log('revObjArr', revObjArr);
-
-        //     resp.status(200).send({
-        //       reviews: revArr
-        //     });
-        //   })
-        //   .catch(err => {
-        //     throw err;
-        //   });
-
-
-
-        // Review.findById(revArr[0], (err, data) => {
-        //   if (err) { throw err; }
-        //   else {
-
-        //     data = {
-        //       ...data._doc,
-        //       product_id: req.params.product_id,
-        //       product_options: prodOpt
-        //     };
-
-        //     resp.status(200).send({
-        //       reviews: data
-        //     });
-        //     return data;
-        //   }
-        // })
 
       })
       .catch((err) => {
