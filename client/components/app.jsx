92beaec1433f34330710ba788cc729def8c066c1
Paul Ferguson <paullferguson@gmail.com>
2020-05-05 14:22:47 -0700

    Tidt the App styled components into a dedicated file

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,33 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ReviewList from "./ReviewList";
-import styled, { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-@font-face {
-  font-family: "Amazon Ember";
-  src: url("https://m.media-amazon.com/images/G/01/AUIClients/AmazonUIFont-amazonember_rg-cc7ebaa05a2cd3b02c0929ac0475a44ab30b7efa._V2_.woff2");
-}
-
-@font-face {
-  font-family: "Amazon Ember";
-  font-weight: 700;
-  src: url("https://m.media-amazon.com/images/G/01/AUIClients/AmazonUIFont-amazonember_bd-46b91bda68161c14e554a779643ef4957431987b._V2_.woff2");
-}
-`
-
-const Wrapper = styled.div`
-  font-size: 13px;
-  line-height: 19px;
-  color: #111;
-  font-family: "Amazon Ember",Arial,sans-serif;
-`
-
-const Msg = styled.p`
-  text-align: center;
-  margin: 50px 20px;
-  color: #555;
-`
+import { GlobalStyle, Wrapper, Msg } from "./app.style";
 
 class App extends React.Component {
   constructor(props) {
