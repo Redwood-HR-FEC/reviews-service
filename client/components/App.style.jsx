e9874b3d05bcd1b6319e46a6515afd89a6a38ec0
Paul Ferguson <paullferguson@gmail.com>
2020-05-07 22:31:17 -0700

    Add backup font source, really slow though

--- client/components/App.style.jsx
+++ client/components/App.style.jsx
@@ -1,18 +1,39 @@
 
 import styled, { createGlobalStyle } from 'styled-components'
 
+// Hotlink source of Amazon Ember, fast  but might die and day
 export const GlobalStyle = createGlobalStyle`
-@font-face {
-  font-family: "Amazon Ember";
-  src: url("https://m.media-amazon.com/images/G/01/AUIClients/AmazonUIFont-amazonember_rg-cc7ebaa05a2cd3b02c0929ac0475a44ab30b7efa._V2_.woff2");
-}
+  @font-face {
+    font-family: "Amazon Ember";
+    src: url("https://m.media-amazon.com/images/G/01/AUIClients/AmazonUIFont-amazonember_rg-cc7ebaa05a2cd3b02c0929ac0475a44ab30b7efa._V2_.woff2");
+  }
 
-@font-face {
-  font-family: "Amazon Ember";
-  font-weight: 700;
-  src: url("https://m.media-amazon.com/images/G/01/AUIClients/AmazonUIFont-amazonember_bd-46b91bda68161c14e554a779643ef4957431987b._V2_.woff2");
-}
+  @font-face {
+    font-family: "Amazon Ember";
+    font-weight: 700;
+    src: url("https://m.media-amazon.com/images/G/01/AUIClients/AmazonUIFont-amazonember_bd-46b91bda68161c14e554a779643ef4957431987b._V2_.woff2");
+  }
 `
+// Backup source of Amazon Ember, slow to load
+// @font-face {
+//   font-family: "Amazon Ember";
+//   src: url("//db.onlinewebfonts.com/t/157c6cc36dd65b1b2adc9e7f3329c761.eot");
+//   src: url("//db.onlinewebfonts.com/t/157c6cc36dd65b1b2adc9e7f3329c761.eot?#iefix") format("embedded-opentype"),
+//   url("//db.onlinewebfonts.com/t/157c6cc36dd65b1b2adc9e7f3329c761.woff2") format("woff2"),
+//   url("//db.onlinewebfonts.com/t/157c6cc36dd65b1b2adc9e7f3329c761.woff") format("woff"),
+//   url("//db.onlinewebfonts.com/t/157c6cc36dd65b1b2adc9e7f3329c761.ttf") format("truetype"),
+//   url("//db.onlinewebfonts.com/t/157c6cc36dd65b1b2adc9e7f3329c761.svg#Amazon Ember") format("svg");
+// }
+// @font-face {
+//   font-family: "Amazon Ember";
+//   font-weight: 700;
+//   src: url("//db.onlinewebfonts.com/t/02fcddff61b677f4989d2933d574ca89.eot");
+//   src: url("//db.onlinewebfonts.com/t/02fcddff61b677f4989d2933d574ca89.eot?#iefix") format("embedded-opentype"),
+//   url("//db.onlinewebfonts.com/t/02fcddff61b677f4989d2933d574ca89.woff2") format("woff2"),
+//   url("//db.onlinewebfonts.com/t/02fcddff61b677f4989d2933d574ca89.woff") format("woff"),
+//   url("//db.onlinewebfonts.com/t/02fcddff61b677f4989d2933d574ca89.ttf") format("truetype"),
+//   url("//db.onlinewebfonts.com/t/02fcddff61b677f4989d2933d574ca89.svg#Amazon Ember") format("svg"); }
+
 
 export const Wrapper = styled.div`
   font-size: 13px;
